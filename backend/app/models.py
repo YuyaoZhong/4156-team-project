1c60f79cb7584986d770081e5a2c485950d70f22
Xiaobin Lin <xl2894@columbia.edu>
2020-11-15 21:03:05 -0500

    taskApi initial commit

--- backend/app/models.py
+++ backend/app/models.py
@@ -7,6 +7,28 @@ class Task(db.Model):  # pylint: disable=too-few-public-methods
     name = db.Column(db.String(256), nullable=False)
     status = db.Column(db.Integer) # null = incomplete (0, 1)
 
+    def toDict(self):
+        task = {
+            "id": self.id,
+            "userId": self.userId,
+            "taskListId": self.taskListId,
+            "name": self.name,
+            "status": self.status
+        }
+        return task
+
+    def update(self, data):
+        for key, value in data.items():
+            if key == "userId":
+                self.userId = value
+            elif key == "taskListId":
+                self.taskListId = value
+            elif key == "name":
+                self.name = value
+            elif key == "status":
+                self.status = value
+        return
+
 class TaskList(db.Model):  # pylint: disable=too-few-public-methods
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     userId = db.Column(db.String(256), nullable=False)
--- backend/app/routes/tasks.py
+++ backend/app/routes/tasks.py
@@ -1,9 +1,96 @@
+from flask import request, jsonify
 from app.routes import routes
+from app.ext import db
+from app.models import Task
+from app.models import TaskList
+from app.utls.apiStatus import apiStatus
+from app.utls.utilities import judgeKeysCorrect
+
 
 @routes.route('/tasks')
 def testTask():
     return "tasks url"
 
 @routes.route('/tasks/<taskId>', methods=["GET"])
-def getTaskById(taskId):
-    return taskId
+def getTask(taskId):
+    code, msg, result = 0, '', {'data': None}
+    targetTask = Task.query.get(id)
+    if not targetTask:
+        code, msg = 404, apiStatus.getReponseMsg(404)
+    else:
+        result["data"] = targetTask.toDict()
+        code, msg = 200, apiStatus.getReponseMsg(200)
+    result["code"] = code
+    result["message"] = msg
+    
+    return jsonify(result)
+
+@routes.route('/tasks/<taskId>', methods=['DELETE'])
+def deleteTask(taskId):
+    code, msg, result = 0, '', {'data': None}
+    targetTask = Task.query.get(taskId)
+    if not targetTask:
+        code, msg = 404, apiStatus.getReponseMsg(404)
+    else:
+        try:
+            db.session.delete(targetTask)
+            db.session.commit()
+            code, msg = 200, apiStatus.getReponseMsg(200)
+        except:
+            code, msg = 500, apiStatus.getReponseMsg(500)
+    result["code"] = code
+    result['message'] = msg
+
+    return jsonify(result)
+
+@routes.route('/tasks', methods=['POST'])
+def createTasks():
+    data = request.get_json()
+    postAttrs = ['userId', 'taskListId', 'name', 'status' ]
+    code, msg, result = 0, '', {'data': None}
+    if not data:
+        code, msg = 400, apiStatus.getReponseMsg(400)
+    elif not judgeKeysCorrect(data, postAttrs):
+        code, msg = 400, apiStatus.getReponseMsg(400)
+    else:
+        try:
+            userId = data['userId']
+            taskListId = data['taskListId'] if 'taskListId' in data else -1
+            name = data['name']
+            status = data['status']
+            newTask = Task(userId=userId, taskListId=taskListId, name=name, status=status)
+            db.session.add(newTask)
+            db.session.commit()
+            result['data'] = newTask.toDict()
+            code, msg = 201, apiStatus.getReponseMsg(201)
+        except:
+            code, msg = 500, apiStatus.getReponseMsg(500)
+    result['code'] = code
+    result['message'] = msg
+    return jsonify(result)
+
+@routes.route('/tasks/<taskId>', method=['PUT'])
+def putTask(taskId):
+    data = request.get_json()
+    postAttrs = ['userId', 'taskListId', 'name', 'status' ]
+    code, msg, result = 0, "", {'data': None}
+    if not data:
+        code, msg = 400, apiStatus.getReponseMsg(400)
+    elif not judgeKeysCorrect(data, postAttrs):
+        code, msg = 400, apiStatus.getReponseMsg(400)
+    else:
+        targetTask = Task.query.get(taskId)
+        if not targetTask:
+            code, msg = 404, apiStatus.getReponseMsg(404)
+        else:
+            try:
+                targetTask.update(data)
+                db.session.commit()
+                result['data'] = targetTask.toDict()
+                code, msg = 201, apiStatus.getReponseMsg(201)
+            except:
+                code, msg = 500, apiStatus.getReponseMsg(500)
+    result['code'] = code
+    result['message'] = msg
+    return jsonify(result)
+
