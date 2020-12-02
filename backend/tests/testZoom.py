6da802b4a17cb9977d407a533cc444ac8be2ad11
Scott Zhu <zz2676@columbia.edu>
2020-12-02 18:22:31 -0500

    Update unit tests

--- backend/tests/testZoom.py
+++ backend/tests/testZoom.py
@@ -2,36 +2,41 @@
 """
 
 import unittest
-from flask import json
+import pprint
 from tests.testCase import TestCase
 
+# Need to manually get valid OAuths
+VALID_OAUTH_0 = ""
+VALID_OAUTH_1 = ""
 
-class TestZoom(unittest.TestCase):
+class TestZoom(TestCase):
     def testZoomIntegration(self):
         """ Test to successfully create a meeting based on provided timer ID """
         response = self.testApp.put('/zoom' +
-                                    '?code=MKdPJy49zC_PuUng-ySRZiNHsNYmaTNbg' +
-                                    '?state=' + "sampleTimerId")
+                                    '?code=' + VALID_OAUTH_0 +
+                                    '&state=' + "38")
         r = response.get_json()
+        print(r)
         self.assertTrue(r['code'] == 200)
-        self.assertTrue('start_url' in r)
-        self.assertTrue('join_url' in r)
+        self.assertTrue('start_url' in r['data'])
+        self.assertTrue('join_url' in r['data'])
 
 
     def testZoomIntegrationInvalidOAuth(self):
         """ Test to create a meeting with invalid OAuth code """
         response = self.testApp.put('/zoom' +
                                     '?code=0' +
-                                    '?state=0')
+                                    '&state=0')
         r = response.get_json()
         self.assertFalse(r['code'] == 200)
         self.assertFalse(r['code'] == 201)
 
+
     def testZoomIntegrationInvalidTimer(self):
         """ Test to create a meeting with invalid OAuth code """
         response = self.testApp.put('/zoom' +
-                                    '?code=MKdPJy49zC_PuUng-ySRZiNHsNYmaTNbg' +
-                                    '?state=0')
+                                    '?code=' + VALID_OAUTH_1 +
+                                    '&state=0')
         r = response.get_json()
         self.assertFalse(r['code'] == 200)
         self.assertFalse(r['code'] == 201)
