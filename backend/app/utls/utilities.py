2f6df0d0970f8c0e98c3bbe4016c0f3d4331a318
Scott Zhu <zz2676@columbia.edu>
2020-12-03 22:04:36 -0500

    Add method for input validity test

--- backend/app/utls/utilities.py
+++ backend/app/utls/utilities.py
@@ -21,6 +21,17 @@ def judgeKeysCorrect(data, attrs):
             return False
     return True
 
+def judgeInputValid(data):
+    if not data:
+        return False
+    for key, value in data.items():
+        if isinstance(value, str):
+            return (len(value) <= 140)
+        elif isinstance(value, int):
+            return (value >= -1000) and (value < 65536)
+        else:
+            return False
+
 
 SQL_DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
 
