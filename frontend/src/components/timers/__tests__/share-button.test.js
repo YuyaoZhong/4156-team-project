8d7a35657b2223d55065bd101f30d300c5bb1ea6
YuyaoZhong <yz3618@columbia.edu>
2020-12-05 17:43:46 -0800

    modify share button

--- frontend/src/components/timers/__tests__/share-button.test.js
+++ frontend/src/components/timers/__tests__/share-button.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'jest-enzyme';
 import Enzyme, { shallow, mount} from 'enzyme'
 import { configure } from "enzyme";
+import { act } from 'react-dom/test-utils';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import renderer from 'react-test-renderer';
 import { DataContext} from '../../../context/data-context';
@@ -13,29 +14,54 @@ configure({ adapter: new Adapter() });
 describe("test <ShareButton/>", () => {
     // mockShareButton;
     let wrapper;
+    const messageLocator = { className: "ui bottom center popup transition visible"};
 
-    // it("test click shareButton and wait", async () => {
-    //
-    //     wrapper = mount(<DataContext.Provider>
-    //         <ShareButton timerId = '111' userId = 'test' />
-    //     </DataContext.Provider>);
-    //     wrapper.find({color:'grey', floated:'right', size:'big'}).at(0).simulate('click');
-    //     wrapper.update();
-    //     jest.advanceTimersByTime(3000);
-    //     wrapper.update();
-    //     expect(wrapper.find({position:'bottom center'}).length).toEqual(0);
-    // });
+    beforeEach(()=>{
+        jest.spyOn(window, 'prompt').mockImplementation(() => {});
+    });
+
+    it("test click shareButton and wait", async () => {
+    
+        jest.useFakeTimers();
+        wrapper = mount(<ShareButton timerId = '111' userId = 'test' />);
+
+        expect(wrapper.find(messageLocator)).not.toExist();
+
+        await act(async ()=>{
+            await wrapper.find({color:'grey', floated:'right', size:'big'}).at(0).simulate('click');
+            wrapper = wrapper.update();
+       });
+
+       expect(wrapper.find(messageLocator)).toExist();
+
+        await act(async ()=>{
+            jest.advanceTimersByTime(3000);
+            wrapper = wrapper.update();
+        });
+
+        expect(wrapper.find(messageLocator)).not.toExist();
+    });
 
     it("test click shareButton twice", async () => {
+      
+        wrapper = mount(<ShareButton timerId = '111' userId = 'test' />);
+       
+            expect(wrapper.find(messageLocator)).not.toExist();
+       
+        await act(async ()=>{
+            await wrapper.find({color:'grey', floated:'right', size:'big'}).at(0).simulate('click');
+            wrapper = wrapper.update();
+       });
 
-        wrapper = mount(<DataContext.Provider>
-            <ShareButton timerId = '111' userId = 'test' />
-        </DataContext.Provider>);
-        wrapper.find({color:'grey', floated:'right', size:'big'}).at(0).simulate('click');
-        wrapper.update();
-        wrapper.find({color:'grey', floated:'right', size:'big'}).at(0).simulate('click');
-        wrapper.update();
-        expect(wrapper.find({position:'bottom center'}).length).toEqual(1);
+       expect(wrapper.find(messageLocator)).toExist();
+
+       await act(async()=>{
+            await wrapper.find({color:'grey', floated:'right', size:'big'}).at(0).simulate('click');
+            wrapper = wrapper.update();
+       });
+
+       expect(wrapper.find(messageLocator)).not.toExist();
+        
     });
 
 });
