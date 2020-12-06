011e6cdbdaa542cb161d870333198ea07410cc43
weihan chen <wc2681@columbia.edu>
2020-12-05 20:43:21 -0500

    timer-message

---
+++ frontend/src/components/timers/__tests__/timer-message.test.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import 'jest-enzyme';
+import Enzyme, { shallow, mount} from 'enzyme'
+import { configure } from "enzyme";
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import renderer from 'react-test-renderer';
+import { DataContext} from '../../../context/data-context';
+import AddedTimerMessage from '../../timers/timer-message';
+
+
+
+configure({ adapter: new Adapter() });
+
+describe("test <AddedTimerMessage/>", () => {
+    // mockTimerMessage;
+    let wrapper;
+    let handleClose;
+    const messageStatus1 = {
+        open: true,
+        success: true,
+    }
+
+    const messageStatus2 = {
+        open: true,
+        success: false,
+    }
+
+    const messageStatus3 = {
+        open: true,
+        success: ,
+    }
+
+    beforeEach(() => {
+        handleClose = jest.fn();
+    });
+
+    it("test true ", async () => {
+
+        wrapper = mount(<DataContext.Provider value = {{}}>
+            <AddedTimerMessage handleClose = {handleClose} messageStatus = {messageStatus1} />
+        </DataContext.Provider>);
+        expect(wrapper.find({className: "ui positive message scale animating in visible transition"}).length).toEqual(1);
+    });
+
+    it("test false ", async () => {
+
+        wrapper = mount(<DataContext.Provider value = {{}}>
+            <AddedTimerMessage handleClose = {handleClose} messageStatus = {messageStatus2} />
+        </DataContext.Provider>);
+        expect(wrapper.find({className: "ui negative message scale animating in visible transition"}).length).toEqual(1);
+    });
+
+    it("test null ", async () => {
+
+        wrapper = mount(<DataContext.Provider value = {{}}>
+            <AddedTimerMessage handleClose = {handleClose} messageStatus = {messageStatus3} />
+        </DataContext.Provider>);
+        expect(wrapper.find({className: "ui negative message scale animating in visible transition"}).length).toEqual(0);
+        expect(wrapper.find({className: "ui positive message scale animating in visible transition"}).length).toEqual(0);
+    });
+});
+
