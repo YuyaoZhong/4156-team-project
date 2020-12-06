1610351b5ebbb2903a6e7db60aa669d5f288ec81
weihan chen <wc2681@columbia.edu>
2020-12-06 01:21:39 -0500

    timer running!

--- frontend/src/components/timers/__tests__/timer-running.test.js
+++ frontend/src/components/timers/__tests__/timer-running.test.js
@@ -9,6 +9,7 @@ import {TimerDetailAttr} from '../../timers/timer-detail-info';
 import {mockRelatedTasksForTimer} from '../../../utilities/mockData';
 import {mockEditTimer} from '../../../utilities/mockData';
 import RunningTimerContainer from "../timer-running";
+import {act} from "react-dom/cjs/react-dom-test-utils.development";
 
 
 configure({ adapter: new Adapter() });
@@ -20,22 +21,38 @@ describe("test <RunningTimerContainer/>", () => {
     // mockTimerDetail;
     let wrapper;
 
-    // it("test huge size ", async () => {
+    const mockTimer = {
+        breakTime: 5,
+        description: "",
+        duration: 25,
+        id: -4,
+        isCreator: false,
+        round: 1,
+        startTime: new Date(),
+        timerToUserId: "0",
+        title: "test shared timer",
+        userId: "-1",
+        zoomLink: "None"
+    }
+
+    // it("test no timer ", async () => {
     //     wrapper = mount(<DataContext.Provider value = {{
+    //         timerRun: undefined
     //      }}>
     //         <RunningTimerContainer/>
     //     </DataContext.Provider>);
-    //
-    //     expect(wrapper.find({size: "huge"}).length).toEqual(2);
+    //     console.log(wrapper.debug());
+    //     expect(wrapper.find({size: "huge"}).length).toEqual(0);
     // });
 
-    it("test no timer ", async () => {
-        wrapper = mount(<DataContext.Provider value = {{
+    it("test timer running ", async () => {
+        await act (async ()=>{wrapper = mount(<DataContext.Provider value = {{
+            timerRun: mockTimer
          }}>
             <RunningTimerContainer/>
         </DataContext.Provider>);
-        console.log(wrapper.debug());
-        expect(wrapper.find({size: "huge"}).length).toEqual(2);
+        console.log(wrapper.debug())});
+        expect(wrapper.find({size: "huge"}).length).toEqual(0);
     });
 
 
