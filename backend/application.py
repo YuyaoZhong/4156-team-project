9ee697e8dc4fe86369460d08c08ac0cf6fc651d4
YuyaoZhong <yz3618@columbia.edu>
2020-12-06 17:29:40 -0800

    modify setting for deploy

--- backend/application.py
+++ backend/application.py
@@ -8,4 +8,6 @@ def index():
     return 'API backend test'
 
 if __name__ == '__main__':
-    app.run(debug=True, ssl_context=('server.crt', 'server.key'), port=8000)
+    # app.run(debug=True, ssl_context=('server.crt', 'server.key'), port=5050)
+
+    app.run(port = 8000, debug = False)
